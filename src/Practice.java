66d8dabf5da052d6b98f63f3521093d122d0d723
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-09-23 21:05:41 -0700

    answered next question

--- src/Practice.java
+++ src/Practice.java
@@ -164,7 +164,15 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if(root == null){
+            return 0;
+        }
+        int leftLevels = levelCount(root.left);
+        int rightLevels = levelCount(root.right);
+
+        int biggest = Math.max(leftLevels, rightLevels);
+
+        return biggest + 1;
     }
 
 
