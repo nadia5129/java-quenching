49dad5f0311857563537493de323277f51584032
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-09-23 20:25:50 -0700

    completed question 4

--- src/Practice.java
+++ src/Practice.java
@@ -104,7 +104,21 @@ public class Practice {
      * @throws IllegalArgumentException if head is null
      */
     public static int biggestNumber(ListNode<Integer> head) {
-        return 0;
+        if( head == null){
+            throw new IllegalArgumentException();
+        }
+
+        int biggest = head.data;
+
+        ListNode<Integer>  current = head;
+
+        while( current != null ){
+            if( current.data > biggest){
+                biggest = current.data;
+    
+            }
+             current = current.next;
+        }
     }
 
     /**
